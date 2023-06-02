55616a3a34080bb366cdb6f0ca5abd2189f7c304
tbeachill <82418020+tbeachill@users.noreply.github.com>
2023-06-02 13:05:59 +0100

    finished exercise

---
+++ 1-5.scm
@@ -0,0 +1,23 @@
+#lang simply-scheme
+
+(define (p) (p))
+
+(define (test x y)
+    (if (= x 0)
+        0
+        y))
+
+(test 0 (p))
+
+#|
+applicative-order evaluation:
+    p is evaluated first, which evaluates p recursively infinitely.
+    test is never evaluated and there is no output.
+
+normal-order evaluation:
+    (if (= 0 0)
+        0
+        (p))
+
+    test is evaluated first and x = 0 = #t so 0 is returned and p is never evaluated.
+|#
